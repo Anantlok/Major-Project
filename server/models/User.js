fe36c44a13fabd736002ac53f1b531673ae944ef
AnantLok <anant.alomano@gmail.com>
2025-11-05 07:41:38 +0530

    okay

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -15,25 +14,7 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  role: {
-    type: String,
-    enum: ['user', 'admin'], // Allowed values
-    default: 'user', // Default for new signups
-  },
 });
 
-// This "pre-save hook" runs before a user is saved to the database
-UserSchema.pre('save', async function(next) {
-  // Only hash the password if it's new or has been modified
-  if (!this.isModified('password')) {
-    return next();
-  }
-
-  // Generate a "salt" to make the hash more secure
-  const salt = await bcrypt.genSalt(10);
-  // Hash the password and replace the plain-text one
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
-module.exports = mongoose.model('User', UserSchema);
+// ✅ SAFE MODEL CREATION
+module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
